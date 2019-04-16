520663b7f0d2c505bd11eb69dcfd43a07a3a0768
Dillon Christensen <dillon.christensen@kalibrate.com>
2019-04-16 14:26:43 -0500

    added bash script and console output

---
+++ resume.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+BOLD='\033[1m'
+HILIGHT='\033[3m'
+UNDERLINE='\033[4m'
+NORM='\033[m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+BLUE='\033[1;34m'
+CYAN='\033[1;36m'
+HR="\n------------------------------------------------------\n"
+
+echo -e $HR;
+echo -e "${GREEN}Dillon Christensen${NORM}";
+echo -e "${UNDERLINE}${BLUE}j.dillon.christensen@gmail.com";
+echo "https://github.com/dillonchr";
+echo -e "${NORM}918-896-7033";
+echo
+echo
+echo "Product developer with experience across browsers, OSes, devices, and architectural layers. Specializing in frontend development with React & Redux using RESTful APIs. Recognized for being a clear and effective communicator and mediating software disciplines and stakeholder concerns. Having a love of learning by doing has led me to the career I love today and the greatest part is I always have new opportunities to learn both from new projects and the people I work with.";
+echo -e $HR;
+echo -e "${CYAN}Kalibrate${NORM} - Tulsa";
+echo -e "${UNDERLINE}Global retail fuel pricing and planning strategic advisors";
+echo -e $NORM;
+echo
+echo -e "${RED}Senior UX Developer${NORM} - 2017-2019";
+echo "Built front-end of flagship planning Saas app and designed UI/UX for multiple platforms";
+echo
+echo -e "${CYAN}*${NORM} Developed a React-based SaaS app using Leaflet and Google Maps";
+echo -e "${CYAN}*${NORM} Debugged memory and performance issues with drawing thousands of markers on a world map";
+echo -e "${CYAN}*${NORM} Designed wireframes and low-to-mid fidelity mockups for new products";
+echo -e "${CYAN}*${NORM} Learned React Native to build an Android app with which users could survey sites across the globe";
+echo -e $HR;
+echo -e "${CYAN}ConsumerAffairs${NORM} / ${CYAN}Gitwit${NORM} - Tulsa";
+echo -e "${UNDERLINE}Online marketplace for consumers to research, rate, and review big purchases${NORM}";
+echo
+echo -e "${RED}Software Engineer Team Lead${NORM} - 2016-2017";
+echo "Led full-stack development of 8 with a focus on SCRUM/Agile methodologies";
+echo
+echo -e "${CYAN}*${NORM} Developed features and fixed defects in public-facing pages using  Javascript, Sass, and Jinja";
+echo -e "${CYAN}*${NORM} Wrote unit tests for changes using Jasmine to ensure code quality in addition to peer reviews";
+echo -e "${CYAN}*${NORM} Forged close relationships with team members, local and remote, to help in their growth and to learn from them as well";
+echo -e "${CYAN}*${NORM} Overhauled several key pages of consumeraffairs.com by working closely with remote design team";
+echo
+echo -e "${RED}Front-end Developer${NORM} - 2016";
+echo "Developed front-end UIs for designs by Gitwit for ConsumerAffairs and other clients";
+echo
+echo -e "${CYAN}*${NORM} Built responsive interactive content modules to be included in high traffic marketing pages";
+echo -e "${CYAN}*${NORM} Prototyped client web-apps using AngularJS";
+echo -e "${CYAN}*${NORM} Interviewed and managed independent contract developers on overflow projects";
+echo -e $HR;
+echo -e "${CYAN}Kalibrate${NORM} - Tulsa";
+echo
+echo -e "${RED}Software Engineer${NORM} - 2014-2016";
+echo "Built new features and maintained legacy web-apps for planning organization";
+echo
+echo -e "${CYAN}*${NORM} Implemented new features in the flagship AngularJS product with many being map-centered improvements";
+echo -e "${CYAN}*${NORM} Learned Test-driven development and implemented process to easily unit-test JS as well as write Protractor tests for E2E";
+echo -e "${CYAN}*${NORM} Fixed bugs and developed enhancements to existing iOS/Android app using JS and Cordova";
+echo -e $HR;
+echo -e "${CYAN}iMirus${NORM} - Tulsa";
+echo -e "${UNDERLINE}Online cross-platform digital publisher and content creator${NORM}";
+echo
+echo -e "${RED}Senior Software Developer${NORM} -_2011-2014";
+echo "Developed new digital reader and maintained existing reader apps in multiple environments";
+echo
+echo -e "${CYAN}*${NORM} Learned AngularJS to use in creating new JS-based document reader app to replace Adobe Air app";
+echo -e "${CYAN}*${NORM} Developed core features and fixed bugs with native apps and released builds on iTunes and Google Play/Amazon";
+echo -e "${CYAN}*${NORM} Recruited new developers and led training on orientations";
+echo -e "${CYAN}*${NORM} Built custom web-app for daily tasks with our internal digital editions processes and workflows";
+echo
+
