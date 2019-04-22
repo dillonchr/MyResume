f6a876f0cca4e1e76e19a3244baa1cc6eb53e21b
Dillon Christensen <dillon.christensen@kalibrate.com>
2019-04-22 10:36:55 -0500

    updated windows verzhs

--- resume.cmd
+++ resume.cmd
@@ -12,44 +12,59 @@ echo[
 echo[
 echo Product developer with experience across browsers, operating systems, devices, and architectural layers. Specializing in frontend development with React ^& Redux using RESTful APIs. Recognized for being a clear and effective communicator and mediating software disciplines and stakeholder concerns. Career path has been directed by a love of learning which continues to lead to new opportunities and challenges to learn from different projects and teams.
 CALL :linerule
+echo [32mTechnical Skills[0m
+echo[
+echo [46m*[0m Javascript (ES5/ES6)
+echo [46m*[0m React ^& React Native
+echo [46m*[0m Google Maps JS SDK
+echo [46m*[0m Angular ^& TypeScript
+echo [46m*[0m Unit and end-to-end testing
+echo [46m*[0m Node JS
+echo [46m*[0m Objective-C
+echo [46m*[0m Docker
+echo [46m*[0m Bash
+echo [46m*[0m Git
+CALL :linerule
+echo [32mExperience[0m
+echo[
 echo [46mKalibrate[0m - Tulsa
 echo [4mGlobal retail fuel pricing and planning strategic advisors
 echo[
 echo[
 echo [31mSenior UX Developer[0m - 2017-2019
-echo Built front-end of flagship planning Saas app and designed UI/UX for multiple platforms
+echo Built front-end of flagship planning SaaS app and designed UI/UX for multiple platforms
 echo[
-echo [46m*[0m Developed a [46mReact[0m-based SaaS app using Leaflet and Google Maps
+echo [46m*[0m Developed a [46mReact[0m-based SaaS app using [46mLeaflet[0m to support both [46mGoogle Maps[0m and [46mBing Maps[0m
 echo [46m*[0m Debugged memory and performance issues with drawing thousands of markers on a world map
 echo [46m*[0m Designed wireframes and low-to-mid fidelity mockups for new products
-echo [46m*[0m Learned React Native to build an Android app with which users could survey sites across the globe
+echo [46m*[0m Learned [46mReact Native[0m to build an [46mAndroid[0m app with which users could survey sites across the globe
 CALL :linerule
 echo [46mConsumerAffairs[0m / [46mGitwit[0m - Tulsa
 echo [4mOnline marketplace for consumers to research, rate, and review big purchases[0m
 echo[
 echo [31mSoftware Engineer Team Lead[0m - 2016-2017
-echo Led full-stack development of 8 with a focus on SCRUM/Agile methodologies
+echo Led full-stack development team of 8 with a focus on SCRUM methodologies
 echo[
-echo [46m*[0m Developed features and fixed defects in public-facing pages using  Javascript, Sass, and Jinja
-echo [46m*[0m Wrote unit tests for changes using Jasmine to ensure code quality in addition to peer reviews
-echo [46m*[0m Forged close relationships with team members, local and remote, to help in their growth and to learn from them as well
+echo [46m*[0m Developed features and fixed defects in public-facing pages using [46mJavascript[0m, [46mSass[0m, and [46mJinja[0m
+echo [46m*[0m Wrote unit tests for changes using [46mJasmine[0m to ensure code quality in addition to peer reviews
+echo [46m*[0m Forged close relationships with team members, local and remote, to both learn more and help in their growth
 echo [46m*[0m Overhauled several key pages of consumeraffairs.com by working closely with remote design team
 echo[
 echo [31mFront-end Developer[0m - 2016
-echo Developed front-end UIs for designs by Gitwit for ConsumerAffairs and other clients
+echo Created UIs from designs by Gitwit for ConsumerAffairs and other clients
 echo[
 echo [46m*[0m Built responsive interactive content modules to be included in high traffic marketing pages
-echo [46m*[0m Prototyped client web-apps using AngularJS
+echo [46m*[0m Prototyped client web-apps using [46mAngularJS[0m
 echo [46m*[0m Interviewed and managed independent contract developers on overflow projects
 CALL :linerule
 echo [46mKalibrate[0m - Tulsa
 echo[
 echo [31mSoftware Engineer[0m - 2014-2016
-echo Built new features and maintained legacy web-apps for planning organization
+echo Built new features and maintained legacy web-apps for planning department
 echo[
-echo [46m*[0m Implemented new features in the flagship AngularJS product with many being map-centered improvements
-echo [46m*[0m Learned Test-driven development and implemented process to easily unit-test JS as well as write Protractor tests for E2E
-echo [46m*[0m Fixed bugs and developed enhancements to existing iOS/Android app using JS and Cordova
+echo [46m*[0m Implemented new features in the flagship [46mAngularJS[0m product with many being map-centered improvements
+echo [46m*[0m Learned Test-driven development and used [46mJasmine[0m and [46mKarma[0m to unit-test features as well as [46mProtractor[0m to functionally test entire app experience
+echo [46m*[0m Fixed bugs and developed enhancements to existing [46miOS/Android[0m app using [46mCordova[0m
 CALL :linerule
 echo [46miMirus[0m - Tulsa
 echo [4mOnline cross-platform digital publisher and content creator[0m
@@ -57,10 +72,10 @@ echo[
 echo [31mSenior Software Developer[0m - 2011-2014
 echo Developed new digital reader and maintained existing reader apps in multiple environments
 echo[
-echo [46m*[0m Learned AngularJS to use in creating new JS-based document reader app to replace Adobe Air app
-echo [46m*[0m Developed core features and fixed bugs with native apps and released builds on iTunes and Google Play/Amazon
+echo [46m*[0m Learned [46mAngularJS[0m to build new document reader app to replace legacy [46mAdobe Air[0m app
+echo [46m*[0m Developed core features and fixed bugs with native apps and released builds on [46miTunes[0m and [46mGoogle Play/Amazon[0m
 echo [46m*[0m Recruited new developers and led training on orientations
-echo [46m*[0m Built custom web-app for daily tasks with our internal digital editions processes and workflows
+echo [46m*[0m Built custom web-app for daily tasks with our internal digital edition processes and workflows
 echo[
 
 :linerule
